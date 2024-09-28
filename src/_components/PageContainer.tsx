54bc6dc3be617714aa743c9f065ecb7875cb6bfb
小林 拓都 <takuto-kobayashi@navitime.co.jp>
2024-09-29 03:09:59 +0900

    Initial commit

---
+++ src/_components/PageContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Memo from "./Memo";
+
+function PageContainer() {
+  return (
+    <div>
+      PageContainer
+      <Memo />
+    </div>
+  );
+}
+
+export default PageContainer;
